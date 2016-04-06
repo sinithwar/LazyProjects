7348321cb53c9a558e6b75dfbdcf1616bba2cbbf
sinithwar <sinithwar@yahoo.com>
2016-04-06 14:58:28 -0400

    In this commit, I decided to see if taking of each array element used would result in a better performance on the system. The memory used for the system was almost half of the original. This is the comparison test and I will be pushing a further revised version later on.

--- RubyTypingSimulator.rb
+++ RubyTypingSimulator.rb
@@ -1,73 +1,75 @@
 require 'win32ole'
 require 'win32/sound'
+require 'Benchmark'
 include Win32
 
+def typing_simulator
 	wsh = WIN32OLE.new("WScript.Shell")
 
-	def file_to_array(file)
+	def fileToArray(file)
 		x = []
 		File.foreach("#{file}") do |line|
 			x << line.to_s.split('')
 		end
 		return x.flatten!
 	end
-	tests = file_to_array("file")
+	tests = fileToArray("C:\\xampp\\htdocs\\PhpDolphin\\Script\\includes\\classes.php")
 	
 	sleep 10
 	x = 0
-	y = 1000
+	y = 10000
 	while x <= y do
 		send = tests[x]
-		speed = 0.075
-		if x == y
+		speed = 0.015
+		case send
+		when y
 			print "Test Complete"
-			Sound.play("key.wav")
 			break()
-		elsif tests[x] === "%"
+		when "%"
 			wsh.SendKeys("+{5}")
 			sleep speed
 			x += 1
-		elsif tests[x] === "("
+		when "("
 			wsh.SendKeys("+{9}")
 			sleep speed
 			x += 1
-		elsif tests[x] === ")"
+		when ")"
 			wsh.SendKeys("+{0}")
 			sleep speed
 			x += 1
-		elsif tests[x] === "'"
+		when "'"
 			wsh.SendKeys("{'}")
 			sleep speed
 			x += 1
-		elsif tests[x] === '"'
+		when '"'
 			wsh.SendKeys("+{'}")
 			sleep speed
 			x += 1
-		elsif tests[x] === "?"
+		when "?"
 			wsh.SendKeys("+{/}")
 			sleep speed
 			x += 1
-		elsif tests[x] === ">"
+		when ">"
 			wsh.SendKeys("+{.}")
 			sleep speed
 			x += 1
-		elsif tests[x] === "<"
+		when "<"
 			wsh.SendKeys("+{,}")
 			sleep speed
 			x += 1
-		elsif tests[x] === ":"
+		when ":"
 			wsh.SendKeys("+{;}")
 			sleep speed
 			x += 1
-		elsif tests[x] === "{"
+		when "{"
 			wsh.SendKeys("+{[}")
 			sleep speed
 			x += 1
-		elsif tests[x] === "}"
+		when "}"
 			wsh.SendKeys("+{]}")
 			sleep speed
 			x += 1
-		elsif tests[x].is_a? String
+		when String
 			wsh.SendKeys("#{send}")
 			sleep speed
 			x += 1
@@ -75,3 +77,86 @@ include Win32
 			x += 1
 		end
 	end
+end
+def typing_simulator_2
+	wsh = WIN32OLE.new("WScript.Shell")
+
+	def fileToArray(file)
+		x = []
+		File.foreach("#{file}") do |line|
+			x << line.to_s.split('')
+		end
+		return x.flatten!
+	end
+	tests = fileToArray("C:\\xampp\\htdocs\\PhpDolphin\\Script\\includes\\classes.php")
+	
+	sleep 10
+	x = 0
+	y = 10000
+	while x <= y do
+		send = tests[0]
+		speed = 0.015
+		case send
+		when y
+			print "Test Complete"
+			break()
+		when "%"
+			wsh.SendKeys("+{5}")
+			sleep speed
+			x += 1
+		when "("
+			wsh.SendKeys("+{9}")
+			sleep speed
+			x += 1
+		when ")"
+			wsh.SendKeys("+{0}")
+			sleep speed
+			x += 1
+		when "'"
+			wsh.SendKeys("{'}")
+			sleep speed
+			x += 1
+		when '"'
+			wsh.SendKeys("+{'}")
+			sleep speed
+			x += 1
+		when "?"
+			wsh.SendKeys("+{/}")
+			sleep speed
+			x += 1
+		when ">"
+			wsh.SendKeys("+{.}")
+			sleep speed
+			x += 1
+		when "<"
+			wsh.SendKeys("+{,}")
+			sleep speed
+			x += 1
+		when ":"
+			wsh.SendKeys("+{;}")
+			sleep speed
+			x += 1
+		when "{"
+			wsh.SendKeys("+{[}")
+			sleep speed
+			x += 1
+		when "}"
+			wsh.SendKeys("+{]}")
+			sleep speed
+			x += 1
+		when String
+			wsh.SendKeys("#{send}")
+			sleep speed
+			x += 1
+		else
+			x += 1
+		end
+	tests.shift
+	end
+end
+
+
+Benchmark.bm do |results|
+	results.report { typing_simulator   }
+	results.report { typing_simulator_2 }
+end
